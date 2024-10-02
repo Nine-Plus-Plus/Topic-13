b438735554d8de09065de5f9ae0331834640bc3f
hoangtmse <hoangtmse183217@fpt.edu.vn>
2024-10-02 23:37:21 +0700

    justify connection between Topic and Class

--- mentor_booking/src/main/java/com/project/model/Group.java
+++ mentor_booking/src/main/java/com/project/model/Group.java
@@ -1,15 +1,7 @@
 package com.project.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Data;
@@ -43,4 +35,8 @@ public class Group {
 
     @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Booking> bookings;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private Class aClass;
 }

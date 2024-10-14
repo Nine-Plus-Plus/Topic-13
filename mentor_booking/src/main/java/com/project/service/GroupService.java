e1f0b71fb8100f3fcfb64ac1efb7f502dfe14133
thinhdat0104 <nguyenthinhdat09876@gmail.com>
2024-10-14 08:42:30 +0700

    Added add new / remove member in group

--- mentor_booking/src/main/java/com/project/service/GroupService.java
+++ mentor_booking/src/main/java/com/project/service/GroupService.java
@@ -1,17 +1,16 @@
-
 package com.project.service;
 
 import com.project.dto.GroupDTO;
 import com.project.dto.Response;
+import com.project.dto.StudentsDTO;
 import com.project.enums.AvailableStatus;
+import com.project.enums.GroupRole;
 import com.project.exception.OurException;
 import com.project.model.Group;
-import com.project.model.Projects;
 import com.project.model.Students;
 import com.project.model.Class;
 import com.project.repository.ClassRepository;
 import com.project.repository.GroupRepository;
-import com.project.repository.ProjectsRepository;
 import com.project.repository.StudentsRepository;
 import com.project.ultis.Converter;
 import java.time.LocalDate;
@@ -27,62 +26,61 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GroupService {
+
     @Autowired
     private GroupRepository groupRepository;
-    
+
     @Autowired
     private StudentsRepository studentsRepository;
-    
-    @Autowired
-    private ProjectsRepository projectsRepository;
-    
+
     @Autowired
     private ClassRepository classRepository;
-    
-    public Response createGroup(GroupDTO inputRequest){
+
+    public Response createGroup(GroupDTO inputRequest) {
         Response response = new Response();
-        try{
-            if (groupRepository.findGroup
-        (inputRequest.getGroupName(),inputRequest.getId(), AvailableStatus.ACTIVE).isPresent())
+        try {
+            if (groupRepository.findGroup(inputRequest.getGroupName(), inputRequest.getId(), AvailableStatus.ACTIVE).isPresent()) {
                 throw new OurException("Group name have existed");
+            }
             Group group = new Group();
             group.setGroupName(inputRequest.getGroupName());
             group.setDateCreated(LocalDate.now());
             group.setDateUpdated(LocalDate.now());
-            
+
             Students student = studentsRepository.findById(inputRequest.getStudents().get(0).getId()).get();
+            student.setGroupRole(GroupRole.LEADER);
             List<Students> studentsList = new ArrayList<>();
             studentsList.add(student);
             group.setStudents(studentsList);
-            
+
             group.setProject(null);
-            
+
             Class aClass = classRepository.findById(inputRequest.getClassDTO().getId()).get();
             group.setAClass(aClass);
-            
+
             group.setAvailableStatus(AvailableStatus.ACTIVE);
             groupRepository.save(group);
-            
-            if (group.getId()>0){
+
+            if (group.getId() > 0) {
                 GroupDTO dto = Converter.convertGroupToGroupDTO(group);
                 response.setGroupDTO(dto);
                 response.setStatusCode(200);
                 response.setMessage("Group created successfully");
             }
-            
-        }catch(OurException e){
+
+        } catch (OurException e) {
             response.setStatusCode(400);
             response.setMessage(e.getMessage());
-        }catch(Exception e){
+        } catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error occurred during group creation: " +e.getMessage());
+            response.setMessage("Error occurred during group creation: " + e.getMessage());
         }
         return response;
     }
-    
-    public Response getAllGroups(){
+
+    public Response getAllGroups() {
         Response response = new Response();
-        try{
+        try {
             List<Group> groupList = groupRepository.findByAvailableStatus(AvailableStatus.ACTIVE);
             List<GroupDTO> groupListDTO = null;
             if (groupList != null) {
@@ -96,66 +94,68 @@ public class GroupService {
                 response.setGroupDTOList(groupListDTO);
                 throw new OurException("Cannot find any group");
             }
-        }catch(OurException e){
+        } catch (OurException e) {
             response.setStatusCode(400);
             response.setMessage(e.getMessage());
-        }catch(Exception e){
+        } catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error occured during get all groups "+e.getMessage());
+            response.setMessage("Error occured during get all groups " + e.getMessage());
         }
         return response;
     }
-    
-    public Response getGroupById(Long id){
+
+    public Response getGroupById(Long id) {
         Response response = new Response();
-        try{
+        try {
             Group findGroup = groupRepository.findByIdAndAvailableStatus(id, AvailableStatus.ACTIVE);
-            GroupDTO dto = Converter.convertGroupToGroupDTO(findGroup);
-            response.setGroupDTO(dto);
             if (findGroup != null) {
+                GroupDTO dto = Converter.convertGroupToGroupDTO(findGroup);
+                response.setGroupDTO(dto);
                 response.setStatusCode(200);
                 response.setMessage("Successfully");
             } else {
                 throw new OurException("Cannot find group");
             }
-        }catch(OurException e){
+        } catch (OurException e) {
             response.setStatusCode(400);
             response.setMessage(e.getMessage());
-        }catch(Exception e){
+        } catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error occured during get group "+e.getMessage());
+            response.setMessage("Error occured during get group " + e.getMessage());
         }
         return response;
     }
-    
-    public Response updateGroup(Long id, Group newGroup){
+
+    public Response updateGroup(Long id, Group newGroup) {
         Response response = new Response();
-        try{
+        try {
             Group presentGroup = groupRepository.findById(id)
                     .orElseThrow(() -> new OurException("Cannot find group with id: " + id));
 
-            if (newGroup.getGroupName() != null) presentGroup.setGroupName(newGroup.getGroupName());
+            if (newGroup.getGroupName() != null) {
+                presentGroup.setGroupName(newGroup.getGroupName());
+            }
             presentGroup.setDateUpdated(LocalDate.now());
-            
+
             groupRepository.save(presentGroup);
 
             GroupDTO dto = Converter.convertGroupToGroupDTO(presentGroup);
             response.setGroupDTO(dto);
             response.setStatusCode(200);
             response.setMessage("Group updated successfully");
-        }catch(OurException e){
+        } catch (OurException e) {
             response.setStatusCode(400);
             response.setMessage(e.getMessage());
-        }catch(Exception e){
+        } catch (Exception e) {
             response.setStatusCode(500);
             response.setMessage("Error occurred while updating class: " + e.getMessage());
         }
         return response;
     }
-    
-    public Response deleteGroup(Long id){
+
+    public Response deleteGroup(Long id) {
         Response response = new Response();
-        try{
+        try {
             Group deleteGroup = groupRepository.findById(id)
                     .orElseThrow(() -> new OurException("Cannot find group with id: " + id));
             deleteGroup.setAvailableStatus(AvailableStatus.DELETED);
@@ -163,12 +163,73 @@ public class GroupService {
 
             response.setStatusCode(200);
             response.setMessage("Group deleted successfully");
-        }catch(OurException e){
+        } catch (OurException e) {
             response.setStatusCode(400);
             response.setMessage(e.getMessage());
-        }catch(Exception e){
+        } catch (Exception e) {
             response.setStatusCode(500);
             response.setMessage("Error occurred while deleting group: " + e.getMessage());
+        }
+        return response;
+    }
+
+    public Response addNewMember(Long id, StudentsDTO newMember) {
+        Response response = new Response();
+        try {
+            Group findGroup = groupRepository.findByIdAndAvailableStatus(id, AvailableStatus.ACTIVE);
+            if (findGroup != null) {
+                Students student = studentsRepository.findByIdAndAvailableStatus(newMember.getId(), AvailableStatus.ACTIVE);
+                if (student != null) {
+                    List<Students> studentsList = findGroup.getStudents();
+                    student.setGroupRole(GroupRole.MEMBER);
+                    studentsList.add(student);
+                    studentsRepository.save(student);
+                    groupRepository.save(findGroup);
+                    response.setStatusCode(200);
+                    response.setMessage("New member added successfully");
+                } else {
+                    throw new OurException("Cannot find student");
+                }
+            } else {
+                throw new OurException("Cannot find group");
+            }
+        } catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error occured during get group " + e.getMessage());
+        }
+        return response;
+    }
+
+    public Response removeMember(Long id, StudentsDTO newMember) {
+        Response response = new Response();
+        try {
+            Group findGroup = groupRepository.findByIdAndAvailableStatus(id, AvailableStatus.ACTIVE);
+            if (findGroup != null) {
+                Students student = studentsRepository.findByIdAndAvailableStatus(newMember.getId(), AvailableStatus.ACTIVE);
+                if (student != null) {
+                    List<Students> studentsList = findGroup.getStudents();
+                    student.setGroupRole(null);
+                    studentsList.remove(student);
+                    findGroup.setStudents(studentsList);
+                    studentsRepository.save(student);
+                    groupRepository.save(findGroup);
+                    response.setStatusCode(200);
+                    response.setMessage("Member have been removed");
+                } else {
+                    throw new OurException("Cannot find student");
+                }
+            } else {
+                throw new OurException("Cannot find group");
+            }
+        } catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error occured during get group " + e.getMessage());
         }
         return response;
     }

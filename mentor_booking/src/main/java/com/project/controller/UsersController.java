c630db2a6a899c56de71748ec89a54091ac15367
hoangtmse <hoangtmse183217@fpt.edu.vn>
2024-10-15 13:37:45 +0700

    aws3service

--- mentor_booking/src/main/java/com/project/controller/UsersController.java
+++ mentor_booking/src/main/java/com/project/controller/UsersController.java
@@ -11,14 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
@@ -33,7 +27,13 @@ public class UsersController {
     }
 
     @PostMapping("/admin/create-student")
-    public ResponseEntity<Response> createStudentRequestResponseEntity(@RequestBody CreateStudentRequest createStudentRequest){
+    public ResponseEntity<Response> createStudentRequestResponseEntity(
+            @RequestPart("student") CreateStudentRequest createStudentRequest,
+            @RequestPart(value = "avatarFile", required = false) MultipartFile avatarFile) {
+
+        if (avatarFile != null && !avatarFile.isEmpty()) {
+            createStudentRequest.setAvatarFile(avatarFile);
+        }
         Response response = userService.createStudents(createStudentRequest);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }

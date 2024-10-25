f8e60b36bb7ee13db8962b0c583ede91fd31f146
thinhdat0104 <nguyenthinhdat09876@gmail.com>
2024-10-25 16:38:54 +0700

    Added Meeting Service

--- mentor_booking/src/main/java/com/project/repository/MeetingRepository.java
+++ mentor_booking/src/main/java/com/project/repository/MeetingRepository.java
@@ -1,11 +1,12 @@
 
 package com.project.repository;
 
+import com.project.enums.AvailableStatus;
 import com.project.model.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting , Long>{
-    
+    Meeting findByBookingIdAndAvailableStatus(Long bookingId, AvailableStatus availableStatus);
 }

14a52a7ccf46ea9692a609243ab84effb268d388
thinhdat0104 <nguyenthinhdat09876@gmail.com>
2024-10-27 16:03:49 +0700

    Sort bookings/point histories by date updated/date created

--- mentor_booking/src/main/java/com/project/repository/PointHistoryRepository.java
+++ mentor_booking/src/main/java/com/project/repository/PointHistoryRepository.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 @Repository
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
-    List<PointHistory> findByStudentId(Long studentId);
+    List<PointHistory> findByStudentIdOrderByDateCreatedDesc(Long studentId);
 
-    @Query("SELECT ph FROM PointHistory ph WHERE ph.student.group.id = :groupId")
+    @Query("SELECT ph FROM PointHistory ph WHERE ph.student.group.id = :groupId ORDER BY ph.dateCreated DESC")
     List<PointHistory> findByStudentGroupId(@Param("groupId") Long groupId);
 
-    @Query("SELECT SUM(ph.point) FROM PointHistory ph WHERE ph.student.group.id = :groupId")
+    @Query("SELECT SUM(ph.point) FROM PointHistory ph WHERE ph.student.group.id = :groupId ORDER BY ph.dateCreated DESC")
     Long findTotalPointsByGroupId(@Param("groupId") Long groupId);
 }
